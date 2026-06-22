3b6e06cccc2d19584f9b5929c8fc7f3cce98f997
haydenmacau <haydenmacau@gmail.com>
2026-06-22 15:23:09 +0800

    Update EatCheck.m

--- EatCheck.m
+++ EatCheck.m
@@ -1,5 +1,10 @@
+% Check if snake will eat apple in next move
+% Input: Snake struct contains Body, Dir, Apple
+% Output: true if eat apple, false otherwise
 function result = EatCheck(Snake)
+    % Initialize flag as not eating apple
     result = false;
+    % Get current head pos + direction offset = next head pos, compare with apple pos
     if Snake.Body(1,:) + Snake.Dir == Snake.Apple 
         result = true;
     end
