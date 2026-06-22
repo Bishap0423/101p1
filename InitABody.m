69ed698e3ac9df850e9248ff10496b782150af56
haydenmacau <haydenmacau@gmail.com>
2026-06-22 16:04:08 +0800

    Update InitABody.m

--- InitABody.m
+++ InitABody.m
@@ -1,7 +1,15 @@
+% Create rectangle graphics object for ONE snake body segment
+% Input: n - index of snake body block; Snake - snake struct
+% Output: snake_rec - handle of rectangle graphic
 function snake_rec = InitABody(n, Snake)
+    % Draw a square block for snake part n
     snake_rec = rectangle( ...
-        "FaceColor", [1 1 1], ...
-        "EdgeColor", 'none', ...
+        "FaceColor", [1 1 1], ...    % Fill color white
+        "EdgeColor", 'none', ...     % No border line
+        % Position: [x_left y_bottom width height]
+        % Offset -0.5 to center square on grid coordinate
         'Position', [Snake.Body(n,1)-0.5 Snake.Body(n,2)-0.5 1 1], ...
-        "Parent",fig);
+        "Parent",fig);               % Attach figure to window fig
+                                     % First index n: row number → the n-th segment of snake body
+                                     % Second index 1: column 1 → X coordinate
 end
